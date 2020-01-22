cad552af368c0bedf3421cf4f9277c277885ca43
Robin Speijer <robin@awkward.co>
2020-01-22 11:02:38 +0100

    Add some inline docs for beam appearence

--- Beam/Controllers/AppearanceController.swift
+++ Beam/Controllers/AppearanceController.swift
@@ -8,6 +8,45 @@
 
 import UIKit
 
+/// Observes user appearance preferences, and overrides the app's user interface
+/// style accordingly for the given window.
+class AppearanceController: NSObject {
+    
+    /// The window to override the user interface style of, if needed.
+    weak var window: UIWindow? {
+        didSet {
+            window?.overrideUserInterfaceStyle = AppearanceController.systemOverride
+        }
+    }
+    
+    /// The appropriate system UI style override settings, according to the
+    /// current user preferences.
+    static private var systemOverride: UIUserInterfaceStyle {
+        if UserSettings[.nightModeAutomaticEnabled] {
+            return .unspecified
+        } else if UserSettings[.nightModeEnabled] {
+            return .dark
+        } else {
+            return .light
+        }
+    }
+    
+    override init() {
+        super.init()
+        NotificationCenter.default.addObserver(self, selector: #selector(AppearanceController.userSettingChanged(_:)), name: .SettingsDidChangeSetting, object: nil)
+    }
+    
+    
+    @objc private func userSettingChanged(_ notification: Notification) {
+        guard notification.object as? SettingsKey == SettingsKeys.nightModeEnabled ||
+            notification.object as? SettingsKey == SettingsKeys.nightModeAutomaticEnabled else {
+                return
+        }
+        window?.overrideUserInterfaceStyle = AppearanceController.systemOverride
+    }
+    
+}
+
 func AppearanceValue<T>(light: T, dark: T) -> T {
     switch AppDelegate.shared.appearanceController.window?.traitCollection.userInterfaceStyle {
     case .dark: return dark
@@ -22,39 +61,4 @@ func AppearanceValue(light: UIColor, dark: UIColor) -> UIColor {
         default: return light
         }
     }
-}
-
-/// Controller to observe dark mode setting changes,
-/// and override the system appearance accordingly.
-class AppearanceController: NSObject {
-    
-    weak var window: UIWindow? {
-        didSet {
-            window?.overrideUserInterfaceStyle = AppearanceController.systemOverride
-        }
-    }
-    
-    override init() {
-        super.init()
-        NotificationCenter.default.addObserver(self, selector: #selector(AppearanceController.userSettingChanged(_:)), name: .SettingsDidChangeSetting, object: nil)
-    }
-    
-    static private var systemOverride: UIUserInterfaceStyle {
-        if UserSettings[.nightModeAutomaticEnabled] {
-            return .unspecified
-        } else if UserSettings[.nightModeEnabled] {
-            return .dark
-        } else {
-            return .light
-        }
-    }
-    
-    @objc private func userSettingChanged(_ notification: Notification) {
-        guard notification.object as? SettingsKey == SettingsKeys.nightModeEnabled ||
-            notification.object as? SettingsKey == SettingsKeys.nightModeAutomaticEnabled else {
-                return
-        }
-        window?.overrideUserInterfaceStyle = AppearanceController.systemOverride
-    }
-    
 }
--- Beam/UI/Generic UI/Base Classes/BeamAppearance.swift
+++ Beam/UI/Generic UI/Base Classes/BeamAppearance.swift
@@ -8,21 +8,20 @@
 
 import UIKit
 
-/** Protocol to implement to support different display modes like dark mode. This has been implemented by default by all beam base views/viewcontrollers.
-
-    To implement this protocol, you need to listen for Notification.Name.DisplayModeDidChange notifications. When the notification fires, call displayModeDidChange(newMode: animated:). Implement displayModeDidChange to update UI elements according to the display mode. This method will be called within an animation block whenever appropiate.
-*/
+/// Legacy protocol implemented by UI elements in the app to update UI elements
+/// upon userInterfaceStyle changes. This has been implemented by default by all
+/// beam base views/viewcontrollers.
+///
+/// Don't implement this protocol in new UI elements. Use colors with different
+/// appearance values, and listen to UITraitEnvironment changes.
 protocol BeamAppearance: UITraitEnvironment {
     
-    /// When the display mode (dark or regular mode) has been changed, this method will be called. It will be embedded in an animation block if appropiate.
+    /// Method to be called on userInterfaceStyle changes in `traitCollectionDidChange`.
     func appearanceDidChange()
     
 }
 
 extension BeamAppearance {
-    
     func appearanceDidChange() {}
-    
     var userInterfaceStyle: UIUserInterfaceStyle { traitCollection.userInterfaceStyle }
-    
 }
